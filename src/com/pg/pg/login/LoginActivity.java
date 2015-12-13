419aacedf27d4124cb0190f30bb11bb8c48e4a7b
zzj <zzj@zzj.(none)>
2015-12-13 22:39:29 +0800

    加入注册页面

--- src/com/pg/pg/login/LoginActivity.java
+++ src/com/pg/pg/login/LoginActivity.java
@@ -3,14 +3,29 @@ package com.pg.pg.login;
 import com.pg.pg.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
+	private Button btdenglu;
+	private Button btzhuce;
 	 @Override
 	 protected void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
 		 requestWindowFeature(Window.FEATURE_NO_TITLE);
 		 setContentView(R.layout.activity_login);
+		 btzhuce = (Button)findViewById(R.id.zhuce);
+		 btdenglu = (Button)findViewById(R.id.denglu);
+	        btzhuce.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View view) {				
+					Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+					startActivity(intent); 
+				}
+			 });
 	 }
 }
---
+++ src/com/pg/pg/login/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.pg.pg.login;
+
+import com.pg.pg.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class RegisterActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_register);
+	}
+}

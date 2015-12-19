bc8a974401ab4c6377d24b05081c7d64ae9521cb
zhangzijian86 <zhangzijian86@163.com>
2015-12-19 23:48:55 +0800

    客户端 服务端联通

---
+++ src/com/pg/pg/bean/Ppdr_dailyrecycle.java
@@ -0,0 +1,77 @@
+package com.pg.pg.bean;
+
+public class Ppdr_dailyrecycle {
+
+	private String dailyrecycle_id;//DAILYRECYCLE_ID
+	private String dailyrecycle_user_mobile;//DAILYRECYCLE_USER_MOBILE
+	private String dailyrecycle_date;//DAILYRECYCLE_DATE
+	private String dailyrecycle_week;//DAILYRECYCLE_WEEK
+	private String dailyrecycle_iscycle;//DAILYRECYCLE_ISCYCLE 是否是周期
+	private String dailyrecycle_cycletype;//DAILYRECYCLE_CYCLETYPE 周期类型 每周，每月
+	private String dailyrecycle_isvalid;//DAILYRECYCLE_ISVALID 是否是有效
+	private String dailyrecycle_status;//DAILYRECYCLE_STATUS 状态  1 完成 0未完成
+	private String dailyrecycle_recyclingmanphone;//DAILYRECYCLE_RECYCLINGMANPHONE 回收人手机
+	private String dailyrecycle_finishtime;//DAILYRECYCLE_FINISHTIME 回收人手机
+	
+	public String getDailyrecycle_id() {
+		return dailyrecycle_id;
+	}
+	public void setDailyrecycle_id(String dailyrecycle_id) {
+		this.dailyrecycle_id = dailyrecycle_id;
+	}
+	public String getDailyrecycle_user_mobile() {
+		return dailyrecycle_user_mobile;
+	}
+	public void setDailyrecycle_user_mobile(String dailyrecycle_user_mobile) {
+		this.dailyrecycle_user_mobile = dailyrecycle_user_mobile;
+	}
+	public String getDailyrecycle_date() {
+		return dailyrecycle_date;
+	}
+	public void setDailyrecycle_date(String dailyrecycle_date) {
+		this.dailyrecycle_date = dailyrecycle_date;
+	}
+	public String getDailyrecycle_week() {
+		return dailyrecycle_week;
+	}
+	public void setDailyrecycle_week(String dailyrecycle_week) {
+		this.dailyrecycle_week = dailyrecycle_week;
+	}
+	public String getDailyrecycle_iscycle() {
+		return dailyrecycle_iscycle;
+	}
+	public void setDailyrecycle_iscycle(String dailyrecycle_iscycle) {
+		this.dailyrecycle_iscycle = dailyrecycle_iscycle;
+	}
+	public String getDailyrecycle_cycletype() {
+		return dailyrecycle_cycletype;
+	}
+	public void setDailyrecycle_cycletype(String dailyrecycle_cycletype) {
+		this.dailyrecycle_cycletype = dailyrecycle_cycletype;
+	}
+	public String getDailyrecycle_isvalid() {
+		return dailyrecycle_isvalid;
+	}
+	public void setDailyrecycle_isvalid(String dailyrecycle_isvalid) {
+		this.dailyrecycle_isvalid = dailyrecycle_isvalid;
+	}
+	public String getDailyrecycle_status() {
+		return dailyrecycle_status;
+	}
+	public void setDailyrecycle_status(String dailyrecycle_status) {
+		this.dailyrecycle_status = dailyrecycle_status;
+	}
+	public String getDailyrecycle_recyclingmanphone() {
+		return dailyrecycle_recyclingmanphone;
+	}
+	public void setDailyrecycle_recyclingmanphone(
+			String dailyrecycle_recyclingmanphone) {
+		this.dailyrecycle_recyclingmanphone = dailyrecycle_recyclingmanphone;
+	}
+	public String getDailyrecycle_finishtime() {
+		return dailyrecycle_finishtime;
+	}
+	public void setDailyrecycle_finishtime(String dailyrecycle_finishtime) {
+		this.dailyrecycle_finishtime = dailyrecycle_finishtime;
+	}
+}
--- src/com/pg/pg/login/LoginActivity.java
+++ src/com/pg/pg/login/LoginActivity.java
@@ -1,18 +1,36 @@
 package com.pg.pg.login;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.pg.pg.R;
+import com.pg.pg.main.MainActivity;
+import com.pg.pg.tools.LoadingProgressDialog;
+import com.pg.pg.tools.Operaton;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 	private Button btdenglu;
 	private Button btzhuce;
+	private EditText passwdedit;
+	private EditText namededit;
+	String usermobile;
+	String loginPass;
+	private LoadingProgressDialog dialog;
 	 @Override
 	 protected void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
@@ -20,12 +38,91 @@ public class LoginActivity extends Activity {
 		 setContentView(R.layout.activity_login);
 		 btzhuce = (Button)findViewById(R.id.zhuce);
 		 btdenglu = (Button)findViewById(R.id.denglu);
-	        btzhuce.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View view) {				
-					Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-					startActivity(intent); 
-				}
-			 });
+	     passwdedit = (EditText)findViewById(R.id.passwdedittext);
+	     namededit = (EditText)findViewById(R.id.nameedittext);
+		 btzhuce.setOnClickListener(new OnClickListener() {
+			 @Override
+			 public void onClick(View view) {				
+				 Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+				 startActivity(intent); 
+			 }
+		 });
+		 btdenglu.setOnClickListener(new OnClickListener() {
+			 @Override
+			 public void onClick(View view) {
+				 //					Intent intent = new Intent(AnyCareLoginActivity.this,AnyCareMainActivity.class); 
+				 //					startActivity(intent);
+//				 if(NetUtils.isConnected(getApplicationContext())){
+					 //true为记住密码
+					 usermobile = namededit.getText().toString().trim();
+					 loginPass = passwdedit.getText().toString().trim();
+					 if(!TextUtils.isEmpty(usermobile)&&!TextUtils.isEmpty(loginPass)){
+						 //执行验证的异步操作
+						 new UserLoginAsyncTask().execute(new String[]{usermobile,loginPass});						 
+					 }else{
+						 Toast.makeText(getApplicationContext(), "手机号或者密码不能为空！", Toast.LENGTH_SHORT).show();
+					 }
+//				 }else{
+//					 NetUtils.openSetting(AnyCareLoginActivity.this);
+//				 }
+			 }
+		 });
+		//初始化dialog
+		 dialog=new LoadingProgressDialog(this,"正在加载...");
 	 }
+	 	 
+ 	/**
+ 	 * dis：AsyncTask参数类型：
+ 	 * 第一个参数标书传入到异步任务中并进行操作，通常是网络的路径
+ 	 * 第二个参数表示进度的刻度
+ 	 * 第三个参数表示返回的结果类型
+ 	 * */
+ 	private class UserLoginAsyncTask extends AsyncTask<String, String, String>{
+ 		//任务执行之前的操作
+ 		@Override
+ 		protected void onPreExecute() {
+ 			// TODO Auto-generated method stub
+ 			super.onPreExecute();
+ 			dialog.show();//显示dialog，数据正在处理....
+ 		}
+ 		//完成耗时操作
+ 		@Override
+ 		protected String doInBackground(String... params) {
+ 			// TODO Auto-generated method stub
+ 			try{
+ 				Operaton operaton=new Operaton();
+ 				String result=operaton.login("Login", params[0], params[1]);
+ 				Log.d("=========result===========", result);
+ 				return result;
+ 			}catch(Exception e){
+ 				e.printStackTrace();
+ 				return "false";
+ 			}
+ 		}
+ 		
+ 		@Override
+ 		protected void onProgressUpdate(String... values) {
+ 			// TODO Auto-generated method stub
+ 			super.onProgressUpdate(values);
+ 			
+ 		}
+ 		
+ 		//数据处理完毕后更新UI操作
+ 		@Override
+ 		protected void onPostExecute(String result) {
+ 			// TODO Auto-generated method stub
+ 			super.onPostExecute(result);
+ 			if("success".equals(result)){
+ 				Toast.makeText(getApplicationContext(), "登录成功", Toast.LENGTH_SHORT).show();
+ 				startActivity(new Intent(getApplication(), MainActivity.class));
+ 				overridePendingTransition(R.anim.zoom_enter, R.anim.zoom_exit);
+ 				LoginActivity.this.finish();
+ 			}else if("false".equals(result)){
+ 				Toast.makeText(getApplicationContext(), "用户名密码有误，请重新登录！", Toast.LENGTH_SHORT).show();
+ 			}else if("networkerror".equals(result)){
+ 				Toast.makeText(getApplicationContext(), "网络加载异常", Toast.LENGTH_SHORT).show();
+ 			}
+ 			dialog.dismiss();//dialog关闭，数据处理完毕
+ 		}
+ 	}
 }

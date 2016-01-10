0d4c5f4ad459a781a440cdfede17e5fed8b4a738
zhangzijian86 <zhangzijian86@163.com>
2016-01-10 22:57:28 +0800

    加入主页图标

--- src/com/pg/pg/main/MineActivity.java
+++ src/com/pg/pg/main/MineActivity.java
@@ -1,16 +1,21 @@
 package com.pg.pg.main;
 
+import com.pg.pg.R;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class MineActivity extends Activity{
 	
+	private ViewPager viewPager;  
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
 		TextView tv = new TextView(this);
 		tv.setText("MineActivity");
 		tv.setGravity(Gravity.CENTER);
